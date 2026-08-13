9867b1971f2aee55c7f9b73f4c55dd3f9bb94aef
ShuaCoronel <your_email@gmail.com>
2026-08-13 17:14:17 +0800

    Feat: Guest Hero Page - Updated the layout component for coopguest - used existing component like buttons and tweaked a little - made temporary landing page design - added temporary bg photo
    Note: need branding board and figma design layout
    Note: need to update hero blade and x-layouts
    Note: need to review previous code push

--- resources/views/hero.blade.php
+++ resources/views/hero.blade.php
@@ -1,19 +1,40 @@
 <x-coopguest-layout>
-    <div class="flex flex-row">
-        <a href="{{route('login')}}">
-            <button> login </button>
+     <x-slot name="header">
+        <div class="flex flex-row px-[15%]">
+            
+                <div class="w-full flex justify-between gap-3">
+                    <nav>
+                        <x-application-logo class="h-10 text-red-600" />
 
-        </a>
+                    </nav>
+
+
+                    <nav class="flex items-center gap-7">
+                        <a href="">
+                            Contact
+                        </a>
+
+
+
+                        <x-primary-button class="!bg-blue-500">
+                            <a href="{{route('login')}}">Login</a>
+
+                        </x-primary-button>
+                    </nav>
+                </div>
+            
+            
+        </div>
+    </x-slot>
+
+    <div>
+        <p class="font-bold text-5xl text-center text-white"> 
+            Manage your cooperative in a neat way!
+        </p>
 
     </div>
-    <x-slot name="header">
-        <h1> TESTING </h1>
-    </x-slot>
-    <nav>
-        
 
-    </nav>
+    
 
-    <p>hello</p>
-    <p class="text-red-600 text-lg">testing</p>
+    
 </x-coopguest-layout>
