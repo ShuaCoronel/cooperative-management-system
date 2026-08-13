d87ee9dbaebe5321a886f537dc096b906fd3e958
ShuaCoronel <shuacoronel@gmail.com>
2026-08-13 21:22:26 +0800

    Feat: New Layout - experimenting on layouts - created a test layout and familiarized how to create custom layout
    NOTE: not yet finished, micro changes for easier pick up from where I left off
    NOTE: update the landing coopguest layout
    IMPORTANT: CREATE Figma design to follow
    IMPORTANT: Continue important feature

--- resources/views/hero.blade.php
+++ resources/views/hero.blade.php
@@ -1,51 +1,19 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<x-coopguest-layout>
+    <div class="flex flex-row">
+        <a href="{{route('login')}}">
+            <button> login </button>
 
-    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    {{-- <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script> --}}
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>CoopEx</title>
-</head>
-<body>
-
-
-    <div class="min-h-screen bg-blue-300 align-middle justify-center">
-
-        <div>
-            <h1>
-            test
-
-            </h1>
-        </div>
-
-
-
-
-        <div>
-            <a
-                            href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] bg-blue-500 text-center text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                        >
-                            Log in
-            </a>
-
-        </div>
-
-
-
+        </a>
 
     </div>
+    <x-slot name="header">
+        <h1> TESTING </h1>
+    </x-slot>
+    <nav>
+        
 
+    </nav>
 
-
-
-    
-
-
-
-
-</body>
-</html>
+    <p>hello</p>
+    <p class="text-red-600 text-lg">testing</p>
+</x-coopguest-layout>
